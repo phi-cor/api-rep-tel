6793a6977bed8a5bce9370a037d0ef0f888dd618
phi-cor <philippe.coratti.pro@gmail.com>
2021-01-30 11:28:21 +0100

    CORS autorizations headers via setHeader in app.js added

--- app.js
+++ app.js
@@ -3,9 +3,16 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var setHeader = function (req, res, next) {
+    res.append("Access-Control-Allow-Origin" , "*");
+    res.append("Access-Control-Allow-Headers","Content-Type")
+    res.append("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+    next();
+  };
 var indexRouter = require('./routes/index');
 
 var app = express();
+app.use(setHeader);
 
 app.use(logger('dev'));
 app.use(express.json());
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,17 @@ var router = express.Router();
 var contactController= require('../controllers/contact-controller');
 var bodyParser = require('body-parser');
 
+
+
+
+
 /* GET home page. */
-router.get('/contacts', function(req, res, next) {
+router.get('/contacts',[ function(req, res, next) {
   res.status(200).send({register:contactController.get})
-});
+}]);
 router.post('/contacts/new', function(req, res, next) {
+  
+  
   contactController.new(req.body);
   console.log(contactController.get);
   res.status(204).send();
